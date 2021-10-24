c5556deaff0d567f65735130252ab5b6c248d288
iannyip <ianyip@nullspace.com.sg>
2021-10-24 20:19:28 +0800

    Refactoredsrc/containers/VaccineRegistration/ListVaccinationBooking.js to jsx, funcitonal component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {Route, Switch,BrowserRouter } from 'react-router-dom';
 import VaccineRegistration from './containers/VaccineRegistration/VaccineRegistration.jsx';
-import {VaccineRegistrationListing} from './containers/VaccineRegistration/ListVaccinationBooking';
+import VaccineRegistrationListing from './containers/VaccineRegistration/ListVaccinationBooking.jsx';
 import EditVaccineRegistration from './containers/VaccineRegistration/EditVaccinationBooking.jsx';
 import { NavBar } from './containers/Nav/index.jsx';
 import { Component } from 'react';
